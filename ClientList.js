c872e90770112ac4c2ea4e4a2f2cf80180816e33
Diona Muçiqi <118547127+dionamuciqi@users.noreply.github.com>
2023-12-01 23:20:08 +0100

    Create ClientList.js
    Lista e klienteve u shtua

---
+++ ClientList.js
@@ -0,0 +1,39 @@
+export const knownClients = [
+  {
+    id: 1,
+    ip: "10.180.18.205",
+    name: "Dion Dauti",
+    readPermission: true,
+    writePermission: true,
+    executePermission: true,
+  },
+  {
+    id: 2,
+    ip: "10.180.21.251",
+    name: "Dion Kastrati",
+    readPermission: true,
+    writePermission: true,
+    executePermission: false,
+  },
+  {
+    id: 3,
+    ip: "192.168.1.14",
+    name: "Diona Muçiqi",
+    readPermission: true,
+    writePermission: true,
+    executePermission: false,
+  },
+  {
+    id: 4,
+    ip: "192.345.235.239",
+    name: "Dominik",
+    readPermission: true,
+    writePermission: true,
+    executePermission: false,
+  },
+]
+
+export function resolveClient(ip) {
+  const client = knownClients.find((client) => client.ip === ip)
+  return client ? client.name : ip
+}
